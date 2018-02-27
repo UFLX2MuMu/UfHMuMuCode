07a788f347b277934ba7b3bfc27e60508a249981
Pierluigi Bortignon <bortigno@gmail.com>
2018-02-27 10:58:40 +0100

    updating ntuples directory

--- UFDiMuonsAnalyzer/crab/templates/crab_config.py
+++ UFDiMuonsAnalyzer/crab/templates/crab_config.py
@@ -23,7 +23,7 @@ config.Data.useParent = False
 config.Data.splitting = 'STR'
 config.Data.unitsPerJob = NUM  ## Should take ~10 minutes for 100k events
 
-config.Data.outLFNDirBase = '/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/Moriond17/Mar13_hiM/'
+config.Data.outLFNDirBase = '/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/data_2017_and_mc_fall17/'
 config.Data.publication = False
 config.Data.outputDatasetTag = 'STR'
 
