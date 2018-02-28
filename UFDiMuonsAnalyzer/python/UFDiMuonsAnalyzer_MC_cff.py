472a4d8b3cb322f11df90afd9920cb56217cc1c3
Pierluigi Bortignon <bortigno@gmail.com>
2018-02-28 11:27:06 +0100

    openinig the HLT trigger requirement for MC

--- UFDiMuonsAnalyzer/python/UFDiMuonsAnalyzer_MC_cff.py
+++ UFDiMuonsAnalyzer/python/UFDiMuonsAnalyzer_MC_cff.py
@@ -17,9 +17,8 @@ DiMuons = cms.EDAnalyzer('UFDiMuonsAnalyzer',
                          ## processName  = cms.string("HLT2"),  ## For reHLT MC samples?
                          ## Unprescaled triggers at the end of 2016
                          ## https://cmswbm.web.cern.ch/cmswbm/cmsdb/servlet/TriggerMode?KEY=l1_hlt_collisions2016/v450
-                         trigNames = cms.vstring("HLT_IsoMu22_eta2p1", "HLT_IsoTkMu22_eta2p1", 
-                                                 "HLT_IsoMu24", "HLT_IsoTkMu24", 
-                                                 "HLT_Mu50", "HLT_TkMu50"),
+                         trigNames = cms.vstring("HLT_IsoMu*", "HLT_IsoTkMu*", 
+                                                 "HLT_Mu*", "HLT_TkMu*"),
 
                          trigResults = cms.InputTag("TriggerResults","","HLT"),
                          ## trigResults = cms.InputTag("TriggerResults","","HLT2"),  ## For reHLT MC samples
